70356813403c4681118031a38e4990f7ac0e9a49
ilq1 <andrey_kalashnikov_am@list.ru>
2025-04-05 03:47:59 +0700

    fix RBNode

--- src/main/kotlin/dto/RBNode.kt
+++ src/main/kotlin/dto/RBNode.kt
@@ -1,12 +1,8 @@
 import dto.Node
 
-open class RBNode<K : Comparable<K>, V>(
-    var key: K,
-    var value: V,
-    var left: RBNode<K, V>? = null,
-    var right: RBNode<K, V>? = null,
-    var parent: RBNode<K, V>? = null,
-    var color: Boolean = true,
-){
+class RBNode<K: Comparable<K>, V>(key: K, value: V) : Node<K, V, RBNode<K,V>>(key, value) {
+    var parent: RBNode<K, V>? = null
+    var color: Boolean = true
     fun isLeaf(): Boolean = (this.left == null) && (this.right == null)
 }
+
