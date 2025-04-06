49429c890debc301289f648507841c7043fd5516
Alexey Zarubin <alken1t15@gmail.com>
2025-04-06 22:25:53 +0300

    edit code

--- src/main/kotlin/common/Tree.kt
+++ src/main/kotlin/common/Tree.kt
@@ -2,13 +2,41 @@ package common
 
 import dto.Node
 
+/**
+ * Абстрактный класс для представления дерева.
+ *
+ * @param K Тип ключа, который должен быть сравнимым (должен реализовывать интерфейс Comparable).
+ * @param V Тип значения, которое хранится в узлах дерева.
+ * @param T Тип узла дерева, который должен быть наследником [Node].
+ */
 abstract class Tree<K : Comparable<K>, V, T : Node<K, V, T>> {
+
+    /**
+     * Вставляет новый элемент с указанным ключом и значением в дерево.
+     *
+     * @param key Ключ для вставляемого элемента.
+     * @param value Значение для вставляемого элемента.
+     */
     abstract fun insert(key: K, value: V)
 
+    /**
+     * Ищет значение в дереве по ключу.
+     *
+     * @param key Ключ, по которому нужно найти значение.
+     * @return Значение, соответствующее ключу, или null, если элемент не найден.
+     */
     abstract fun search(key: K): V?
 
+    /**
+     * Защищённая функция для поиска значения в поддереве с корнем [root].
+     * Функция возвращает значение, если ключ найден, или null в противном случае.
+     *
+     * @param root Корень поддерева, в котором нужно выполнить поиск.
+     * @param key Ключ, по которому нужно найти значение.
+     * @return Значение, соответствующее ключу, или null, если элемент не найден.
+     */
     protected fun searchValue(root: T?, key: K): V? {
-        var node = root;
+        var node = root
         while (node != null) {
             node = when {
                 key < node.key -> node.left
@@ -19,10 +47,25 @@ abstract class Tree<K : Comparable<K>, V, T : Node<K, V, T>> {
         return null
     }
 
+    /**
+     * Удаляет элемент по ключу из дерева.
+     *
+     * @param key Ключ элемента, который необходимо удалить.
+     * @return true, если элемент был удален, и false, если элемент не найден.
+     */
     abstract fun delete(key: K): Boolean
 
+    /**
+     * Создает итератор для обхода дерева в ширину (BFS).
+     *
+     * @return Итератор для обхода дерева в ширину.
+     */
     abstract fun treeBFSIterator(): Iterator<T>
 
+    /**
+     * Создает итератор для обхода дерева в глубину (DFS).
+     *
+     * @return Итератор для обхода дерева в глубину.
+     */
     abstract fun treeDFSIterator(): Iterator<T>
-
 }
--- src/main/kotlin/trees/BinarySearchTree.kt
+++ src/main/kotlin/trees/BinarySearchTree.kt
@@ -5,29 +5,52 @@ import dto.BNode
 import iterators.TreeBFSIterator
 import iterators.TreeDFSIterator
 
+/**
+ * Класс [BinarySearchTree] представляет собой реализацию бинарного дерева поиска.
+ *
+ * Бинарное дерево поиска — это структура данных, в которой для каждого узла выполняется условие:
+ * все ключи в левом поддереве узла меньше, а в правом — больше, чем ключ узла.
+ *
+ * @param K Тип ключа, который должен быть сравнимым (реализует интерфейс [Comparable]).
+ * @param V Тип значения, которое хранится в узле дерева.
+ * @constructor Создает пустое бинарное дерево поиска.
+ */
 class BinarySearchTree<K : Comparable<K>, V> : Tree<K, V, BNode<K, V>>() {
 
+    // Корень дерева, изначально равен null
     private var root: BNode<K, V>? = null
 
+    /**
+     * Вставляет новый элемент с заданным ключом и значением в дерево.
+     * Если элемент с таким ключом уже существует, обновляется его значение.
+     *
+     * @param key Ключ элемента, который необходимо вставить.
+     * @param value Значение элемента, которое необходимо вставить.
+     */
     override fun insert(key: K, value: V) {
         if (root == null) {
+            // Если дерево пусто, создаем корень
             root = BNode(key, value)
         } else {
             var node = root
+            // Ищем место для нового узла
             while (node != null) {
                 if (key < node.key) {
+                    // Если ключ меньше текущего, идем в левое поддерево
                     if (node.left == null) {
-                        node.left = BNode(key, value)
+                        node.left = BNode(key, value)  // Вставляем новый узел
                         break
                     }
                     node = node.left
                 } else if (key > node.key) {
+                    // Если ключ больше текущего, идем в правое поддерево
                     if (node.right == null) {
-                        node.right = BNode(key, value)
+                        node.right = BNode(key, value)  // Вставляем новый узел
                         break
                     }
                     node = node.right
-                } else if (key == node.key) {
+                } else {
+                    // Если ключ уже существует, обновляем значение
                     node.value = value
                     break
                 }
@@ -35,7 +58,13 @@ class BinarySearchTree<K : Comparable<K>, V> : Tree<K, V, BNode<K, V>>() {
         }
     }
 
-
+    /**
+     * Удаляет элемент с заданным ключом из дерева.
+     * Если элемент с таким ключом не найден, возвращает false.
+     *
+     * @param key Ключ элемента, который нужно удалить.
+     * @return true, если элемент был успешно удален, иначе false.
+     */
     override fun delete(key: K): Boolean {
         var node = root
         var parent: BNode<K, V>? = null
@@ -46,32 +75,31 @@ class BinarySearchTree<K : Comparable<K>, V> : Tree<K, V, BNode<K, V>>() {
             node = if (key < node.key) node.left else node.right
         }
 
-        if (node == null) return false// Узел не найден
+        if (node == null) return false // Узел не найден
 
         // Если у узла нет правого поддерева
         if (node.right == null) {
+            // Удаляем узел: его левое поддерево поднимается на место удаляемого узла
             if (parent == null) {
-                root = node.left // Удаляемый узел - корень
+                root = node.left
+            } else if (node == parent.left) {
+                parent.left = node.left
             } else {
-                if (node == parent.left) {
-                    parent.left = node.left
-                } else {
-                    parent.right = node.left
-                }
+                parent.right = node.left
             }
         } else {
-            // Находим наименьший узел в правом поддереве (замену)
-            var successor: BNode<K, V>? = node.right
-            var successorParent: BNode<K, V>? = node
+            // Находим наименьший узел в правом поддереве
+            var successor = node.right
+            var successorParent = node
 
-            while (successor?.left != null) { // Безопасный вызов ?.left
+            while (successor?.left != null) {
                 successorParent = successor
                 successor = successor.left
             }
 
-            // Заменяем удаляемый узел найденным узлом
-            if (successorParent != node) {
-                successorParent?.left = successor?.right // Безопасный вызов ?.
+            // Заменяем удаляемый узел найденным узлом (successor)
+            if (successor != node.right) {
+                successorParent?.left = successor?.right
                 successor?.right = node.right
             }
 
@@ -85,18 +113,34 @@ class BinarySearchTree<K : Comparable<K>, V> : Tree<K, V, BNode<K, V>>() {
                 parent.right = successor
             }
         }
-        return false
+
+        return true
     }
 
-
+    /**
+     * Ищет значение по ключу в дереве.
+     *
+     * @param key Ключ элемента, значение которого нужно найти.
+     * @return Значение, соответствующее ключу, или null, если элемент не найден.
+     */
     override fun search(key: K): V? {
         return searchValue(root, key)
     }
 
+    /**
+     * Создает итератор для обхода дерева в ширину (BFS).
+     *
+     * @return Итератор для обхода дерева в ширину.
+     */
     override fun treeBFSIterator(): Iterator<BNode<K, V>> {
         return TreeBFSIterator(root)
     }
 
+    /**
+     * Создает итератор для обхода дерева в глубину (DFS).
+     *
+     * @return Итератор для обхода дерева в глубину.
+     */
     override fun treeDFSIterator(): Iterator<BNode<K, V>> {
         return TreeDFSIterator(root)
     }
